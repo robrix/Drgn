915a4d4c3150e612eba2aef378eb031e47af08d6
Rob Rix <rob.rix@me.com>
2011-09-23 23:23:52 -0400

    The curve is scaled to fit within a 50px inset subregion of the view.

--- Drgn/Controller/DrgnAppDelegate.m
+++ Drgn/Controller/DrgnAppDelegate.m
@@ -10,6 +10,9 @@
 #import "DrgnIteration.h"
 #import <QuartzCore/QuartzCore.h>
 
+@interface DrgnAppDelegate () <NSWindowDelegate>
+@end
+
 @implementation DrgnAppDelegate
 
 @synthesize window, view, iteration;
@@ -27,6 +30,8 @@
 	CGColorRef backgroundColour = CGColorCreateGenericGray(0.5, 1.0);
 	view.layer.backgroundColor = backgroundColour;
 	CGColorRelease(backgroundColour);
+	
+	self.window.delegate = self;
 }
 
 
@@ -61,12 +66,28 @@
 	CGColorRelease(strokeColour);
 	
 	shape.bounds = CGPathGetBoundingBox(shape.path);
-	shape.position = CGPointMake(CGRectGetMidX(view.frame), CGRectGetMidY(view.frame));
+	shape.position = CGPointMake(CGRectGetMidX(view.bounds), CGRectGetMidY(view.bounds));
 	
 	shape.lineWidth = 1;
 	
+	CGRect drawingBounds = CGRectInset(view.bounds, 50, 50);
+	CGFloat scale = MIN(CGRectGetWidth(drawingBounds) / CGRectGetWidth(shape.bounds), CGRectGetHeight(drawingBounds) / CGRectGetHeight(shape.bounds));
+	
+	shape.affineTransform = CGAffineTransformMakeScale(scale, scale);
+	
 	[view.layer addSublayer:shape];
 	[shape release];
+}
+
+
+-(void)windowDidResize:(NSNotification *)notification {
+	[CATransaction begin];
+	[CATransaction setAnimationDuration:0];
+	((CALayer *)self.view.layer.sublayers.lastObject).position = (CGPoint){
+		CGRectGetMidX(view.bounds),
+		CGRectGetMidY(view.bounds),
+	};
+	[CATransaction commit];
 }
 
 @end
